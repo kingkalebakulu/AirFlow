a1effa62133c62bc5ab29a3accb22025614a1f9f
kingkalebakulu <kalebakulu89@gmail.com>
2026-01-08 21:57:23 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from my manual React Repo!</h1>
+      <p>This was built entirely on the GitHub web interface.</p>
+    </div>
+  )
+}
+
+export default App
